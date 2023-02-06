a6accaa28ac47fb8c1a869da9f5154090c985077
mtbhttd <bh.amit82@gmail.co>
2023-02-06 17:12:42 -0500

    Method References

--- src/com/func/Functional03.java
+++ src/com/func/Functional03.java
@@ -13,6 +13,7 @@ public class Functional03 {
                 .map(x -> x*x)
                 .forEach(System.out::println);*/
 
+        // Predicate is used when you have 1 input to your lambda function and you would want a boolean back
         Predicate<Integer> predicate = new Predicate<Integer>() {
             @Override
             public boolean test(Integer number) {
@@ -20,6 +21,7 @@ public class Functional03 {
             }
         };
 
+        // Function will take 1 input and return one output back
         Function<Integer, Integer> function = new Function<Integer, Integer>() {
             @Override
             public Integer apply(Integer number) {
@@ -27,6 +29,7 @@ public class Functional03 {
             }
         };
 
+        // Function will take 1 input and return one output back
         Function<Integer, String> stringOutputFunction = new Function<Integer, String>() {
             @Override
             public String apply(Integer integer) {
@@ -34,6 +37,7 @@ public class Functional03 {
             }
         };
 
+        // Binary operator takes 2 inputs and returns 1 input back
         BinaryOperator<Integer> sumBinaryOperatorImpl = new BinaryOperator<Integer>() {
             @Override
             public Integer apply(Integer baseNumber, Integer additiveNumber) {
@@ -41,7 +45,7 @@ public class Functional03 {
             }
         };
 
-
+        // Consumer takes one input and consumes it
         Consumer<Integer> consumer = new Consumer<Integer>() {
             @Override
             public void accept(Integer number) {
@@ -75,15 +79,22 @@ public class Functional03 {
         System.out.println(sumOfEvenSquares);
         System.out.println(randomIntSupplier.get());
 
+        // BiPredicate is used when you have 2 inputs to your lambda function and you would want a boolean back
         BiPredicate<Integer,String> testStringIntegerEquivalent = (x,y) -> {
           return x == Integer.parseInt(y);
         };
         System.out.println(testStringIntegerEquivalent.test(2,"3"));
 
+        // BiFunction will take 2 inputs and return one output back. Output can be of a different type
         BiFunction<Integer, String, String> appenderBiFunction = (x,y) -> x+y;
         System.out.println(appenderBiFunction.apply(2,"342"));
 
+        // BiConsumer takes two inputs and consumes them
         BiConsumer<Integer,String> biConsumerPrinter = (x,y) -> System.out.println(x+"----"+y);
         biConsumerPrinter.accept(2,"342");
+
+        // IntBinaryOperator takes in two int arguments and return an int.
+        // It increases efficiency by avoiding boxing/unboxing required in generic BinaryOperator
+        IntBinaryOperator intBinaryOperator = (x,y) -> x+y ;
     }
 }
---
+++ src/com/func/MethodReferences01.java
@@ -0,0 +1,27 @@
+package com.func;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+public class MethodReferences01 {
+
+    private static void print(String str){
+        System.out.println(str);
+    }
+    public static void main(String[] args) {
+        List<String> courses = List.of("Spring", "Spring Boot", "API", "MicroServices", "AWS", "PCF",
+                "Azure", "Docker", "Kubernetes");
+
+        /*courses.stream()
+                .map(str->str.toUpperCase())
+                .forEach(str -> System.out.println(str));*/
+
+        courses.stream()
+                .map(String::toUpperCase)
+                //.forEach(System.out::println);
+                .forEach(MethodReferences01::print);
+
+        Supplier<String> supplier = String::new;
+
+    }
+}
